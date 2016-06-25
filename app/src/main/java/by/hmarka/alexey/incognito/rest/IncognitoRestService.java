db520bbe9c4a7c017bb7768c08aa62ba0dc991cc
Lashkevich Andrei <lashket@gmail.com>
2016-06-25 14:12:22 +0300

    Register/postlist request interface

--- app/src/main/java/by/hmarka/alexey/incognito/rest/IncognitoRestService.java
+++ app/src/main/java/by/hmarka/alexey/incognito/rest/IncognitoRestService.java
@@ -1,5 +1,12 @@
 package by.hmarka.alexey.incognito.rest;
 
+import by.hmarka.alexey.incognito.entities.requests.PostsListRequest;
+import by.hmarka.alexey.incognito.entities.requests.RegisterDeviceRequest;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
 /**
  * Created by lashket on 25.6.16.
  */
@@ -14,5 +21,9 @@ public interface IncognitoRestService {
      * call.enqueque(new Callback);
      */
 
+    @POST("registerDevice.xml")
+    Call<ResponseBody> registerDevice(@Body RegisterDeviceRequest registerDeviceRequest);
 
+    @POST("postList.xml")
+    Call<ResponseBody> getPostsList(@Body PostsListRequest postsListRequest);
 }
