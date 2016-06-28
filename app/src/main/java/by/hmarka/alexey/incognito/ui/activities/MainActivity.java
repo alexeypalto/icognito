0f09715a6662b2c27b439debb0beb3f4cb1cf563
Lashkevich Andrei <lashket@gmail.com>
2016-06-28 15:32:27 +0300

    Main activity

--- app/src/main/java/by/hmarka/alexey/incognito/ui/activities/MainActivity.java
+++ app/src/main/java/by/hmarka/alexey/incognito/ui/activities/MainActivity.java
@@ -1,30 +1,66 @@
 package by.hmarka.alexey.incognito.ui.activities;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 import by.hmarka.alexey.incognito.R;
+import by.hmarka.alexey.incognito.ui.adapters.CustomPagerAdapter;
+import by.hmarka.alexey.incognito.ui.fragments.AddFragment;
+import by.hmarka.alexey.incognito.ui.fragments.FavoritesFragment;
+import by.hmarka.alexey.incognito.ui.fragments.HomeFragment;
+import by.hmarka.alexey.incognito.ui.fragments.NewsFragment;
+import by.hmarka.alexey.incognito.ui.fragments.SettingsFragment;
 
 /**
  * Created by Alexey on 22.06.2016.
  */
 public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private CustomPagerAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_activity);
-
-        Toolbar tb = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(tb);
-
-        // Get the ActionBar here to configure the way it behaves.
-        final ActionBar ab = getSupportActionBar();
-        //ab.setHomeAsUpIndicator(R.drawable.ic_menu); // set a custom icon for the default home button
-        ab.setDisplayShowHomeEnabled(true); // show or hide the default home button
-        ab.setDisplayHomeAsUpEnabled(true);
-        ab.setDisplayShowCustomEnabled(true); // enable overriding the default toolbar layout
-        ab.setDisplayShowTitleEnabled(false); // disable the default title element here (for centered title)
+        setContentView(R.layout.activity_main);
+        findViews();
+        setupViewPager(viewPager);
+        setUi();
+        setupTabLayout();
     }
+
+    private void findViews() {
+        tabLayout = (TabLayout) findViewById(R.id.mainTabLayout);
+        viewPager = (ViewPager) findViewById(R.id.mainViewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        adapter = new CustomPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new HomeFragment(), "home");
+        adapter.addFragment(new NewsFragment(), "news");
+        adapter.addFragment(new AddFragment(), "add");
+        adapter.addFragment(new FavoritesFragment(), "favorite");
+        adapter.addFragment(new SettingsFragment(), "settings");
+        viewPager.setAdapter(adapter);
+    }
+
+    private void setUi() {
+        tabLayout.setupWithViewPager(viewPager);
+        viewPager.setOffscreenPageLimit(5);
+    }
+
+    private void setupTabLayout() {
+        tabLayout.getTabAt(0).setCustomView(R.layout.tab_home_layout);
+        tabLayout.getTabAt(1).setCustomView(R.layout.tab_cat_layout);
+        tabLayout.getTabAt(2).setCustomView(R.layout.tab_add);
+        tabLayout.getTabAt(3).setCustomView(R.layout.tab_favorite_layout);
+        tabLayout.getTabAt(4).setCustomView(R.layout.tab_settings_layout);
+        tabLayout.getTabAt(0).getCustomView().setSelected(true);
+    }
+
 }
---
+++ app/src/main/java/by/hmarka/alexey/incognito/ui/adapters/CustomPagerAdapter.java
@@ -0,0 +1,41 @@
+package by.hmarka.alexey.incognito.ui.adapters;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by lashket on 28.6.16.
+ */
+public class CustomPagerAdapter  extends FragmentPagerAdapter {
+
+    private final List<Fragment> fragmentList = new ArrayList<>();
+    private final List<String> fragmentTitleList = new ArrayList<>();
+
+    public CustomPagerAdapter(FragmentManager manager) {
+        super(manager);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragmentList.size();
+    }
+
+    public void addFragment(Fragment fragment, String title) {
+        fragmentList.add(fragment);
+        fragmentTitleList.add(title);
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return null;
+    }
+}
