b159d3ca72a686deaf31b98241a94c9ca26d970f
Lashkevich Andrei <lashket@gmail.com>
2016-07-04 15:17:25 +0300

    Fix errors

--- app/src/main/java/by/hmarka/alexey/incognito/ui/activities/CategoriesActivity.java
+++ app/src/main/java/by/hmarka/alexey/incognito/ui/activities/CategoriesActivity.java
@@ -12,23 +12,23 @@ import by.hmarka.alexey.incognito.R;
  * Created by Alexey on 01.07.2016.
  */
 public class CategoriesActivity extends AppCompatActivity {
-    private CardView mCardView;
-    private CardView.Adapter mAdapter;
-    private CardView.LayoutManager mLayoutManager;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.categories_layout);
-        mCardView = (CardView) findViewById(R.id.card_view);
-
-        // use a linear layout manager
-        mLayoutManager = new LinearLayoutManager(this);
-        mRecyclerView.setLayoutManager(mLayoutManager);
-
-        // specify an adapter (see also next example)
-        mAdapter = new MyAdapter(myDataset);
-        mRecyclerView.setAdapter(mAdapter);
-    }
-    ...
+//    private CardView mCardView;
+//    private CardView.Adapter mAdapter;
+//    private CardView.LayoutManager mLayoutManager;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.categories_layout);
+//        mCardView = (CardView) findViewById(R.id.card_view);
+//
+//        // use a linear layout manager
+//        mLayoutManager = new LinearLayoutManager(this);
+//        mRecyclerView.setLayoutManager(mLayoutManager);
+//
+//        // specify an adapter (see also next example)
+//        mAdapter = new MyAdapter(myDataset);
+//        mRecyclerView.setAdapter(mAdapter);
+//    }
+//    ...
 }
