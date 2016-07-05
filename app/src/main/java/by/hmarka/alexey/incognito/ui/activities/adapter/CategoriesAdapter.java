c2e2102c8bf2c6f14d3ae818e8bcef4610e1d79f
Lashkevich Andrei <lashket@gmail.com>
2016-07-05 15:37:55 +0300

    Favorite post layout

--- app/src/main/java/by/hmarka/alexey/incognito/ui/activities/adapter/CategoriesAdapter.java
+++
@@ -1,7 +0,0 @@
-package by.hmarka.alexey.incognito.ui.activities.adapter;
-
-/**
- * Created by Alexey on 01.07.2016.
- */
-public class CategoriesAdapter {
-}
--- app/src/main/java/by/hmarka/alexey/incognito/ui/fragments/NewPostsFragment.java
+++ app/src/main/java/by/hmarka/alexey/incognito/ui/fragments/NewPostsFragment.java
@@ -3,8 +3,10 @@ package by.hmarka.alexey.incognito.ui.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,11 +21,15 @@ import by.hmarka.alexey.incognito.ui.adapters.PostsAdapter;
 public class NewPostsFragment extends Fragment {
 
     private RecyclerView recyclerView;
+    private Toolbar toolbar;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_new, container, false);
+        toolbar = (Toolbar) view.findViewById(R.id.toolbar);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(R.string.favorite);
         recyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
         recyclerView.setLayoutManager(linearLayoutManager);
