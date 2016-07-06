62777b52a94686f14c5c80530c12abf7eb2a7770
vitalyorlov <vitali.orlov.96@gmail.com>
2016-07-06 16:13:42 +0300

    Add settings list adapter and settings view is implemented

---
+++ app/src/main/java/by/hmarka/alexey/incognito/ui/adapters/SettingsAdapter.java
@@ -0,0 +1,68 @@
+package by.hmarka.alexey.incognito.ui.adapters;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import by.hmarka.alexey.incognito.R;
+import by.hmarka.alexey.incognito.entities.Setting;
+
+/**
+ * Created by vitalyorlov on 06.07.16.
+ */
+public class SettingsAdapter extends BaseAdapter {
+
+    Context ctx;
+    LayoutInflater lInflater;
+    ArrayList<Setting> objects;
+
+    public SettingsAdapter(Context context, ArrayList<Setting> settings) {
+        ctx = context;
+        objects = settings;
+        lInflater = (LayoutInflater) ctx
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return objects.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return objects.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View view = convertView;
+        if (view == null) {
+            view = lInflater.inflate(R.layout.fragment_setting, parent, false);
+        }
+
+        final Setting setting = getSetting(position);
+
+        ((TextView) view.findViewById(R.id.setting_name)).setText(setting.getName());
+        ((ImageView) view.findViewById(R.id.setting_icon)).setImageResource(setting.getImage());
+
+        return view;
+    }
+
+    Setting getSetting(int position) {
+        return ((Setting) getItem(position));
+    }
+
+}
--- app/src/main/java/by/hmarka/alexey/incognito/ui/fragments/SettingsFragment.java
+++ app/src/main/java/by/hmarka/alexey/incognito/ui/fragments/SettingsFragment.java
@@ -1,23 +1,63 @@
 package by.hmarka.alexey.incognito.ui.fragments;
 
+import android.media.audiofx.BassBoost;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 import by.hmarka.alexey.incognito.R;
+import by.hmarka.alexey.incognito.entities.Setting;
+import by.hmarka.alexey.incognito.ui.adapters.SettingsAdapter;
 
 /**
  * Created by lashket on 28.6.16.
  */
 public class SettingsFragment extends Fragment {
 
+    private ArrayList<Setting> settingsList = new ArrayList<Setting>();
+    private SettingsAdapter settingsAdapter;
+    private Toolbar toolbar;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        toolbar = (Toolbar) view.findViewById(R.id.toolbar);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+        if (((AppCompatActivity) getActivity()).getSupportActionBar() != null) {
+            ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle("Настройки");
+        }
+
+        fillSettingsList();
+        settingsAdapter = new SettingsAdapter(this.getContext(), settingsList);
+
+        ListView lvMain = (ListView) view.findViewById(R.id.settingsList);
+        lvMain.setAdapter(settingsAdapter);
+
         return view;
     }
+
+    private void fillSettingsList() {
+        settingsList.add(new Setting(R.drawable.settings_share, "Расскажи друзьям о приложении"));
+
+        settingsList.add(new Setting(R.drawable.settings_notifications, "Уведомления"));
+        settingsList.add(new Setting(R.drawable.settings_shop, "Магазин"));
+        settingsList.add(new Setting(R.drawable.settings_nastroyki, "Настройки"));
+
+        settingsList.add(new Setting(R.drawable.settings_guide, "Правила"));
+        settingsList.add(new Setting(R.drawable.settings_confidenc, "Конфиденциальность"));
+        settingsList.add(new Setting(R.drawable.settings_help, "Помощь"));
+
+        settingsList.add(new Setting(R.drawable.settings_about, "О нас"));
+    }
+
 }
