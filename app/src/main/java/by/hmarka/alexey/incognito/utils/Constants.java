8832d6ce083dd7b0d45fa9f0a264a68838f596c8
Alex Dolgov <alexdolgov1@mail.ru>
2016-07-08 12:07:20 +0300

    add GCM service

--- app/src/main/java/by/hmarka/alexey/incognito/utils/Constants.java
+++ app/src/main/java/by/hmarka/alexey/incognito/utils/Constants.java
@@ -7,4 +7,8 @@ public class Constants {
 
     public static final String BASE_URL = "http://5.45.112.166/";
 
+
+    public static final String SENT_TOKEN_TO_SERVER = "sentTokenToServer";
+    public static final String REGISTRATION_COMPLETE = "registrationComplete";
+    public static final String GCM_TOKEN = "GCM_TOKEN";
 }
