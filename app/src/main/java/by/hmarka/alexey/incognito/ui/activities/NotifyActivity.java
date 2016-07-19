ba5688ec00d9bf50a17a4adfd2e9e8c4a111f930
Alexey Palto <alexey.palto@gmail.com>
2016-07-19 17:00:21 +0300

    -add swipe-to-dismiss

--- app/src/main/java/by/hmarka/alexey/incognito/ui/activities/NotifyActivity.java
+++ app/src/main/java/by/hmarka/alexey/incognito/ui/activities/NotifyActivity.java
@@ -1,15 +1,18 @@
 package by.hmarka.alexey.incognito.ui.activities;
 
-import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.GestureDetector;
-import android.view.MotionEvent;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.View;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -45,8 +48,11 @@ public class NotifyActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(mLayoutManager);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
         recyclerView.setAdapter(mAdapter);
+        setUpItemTouchHelper();
 
-        recyclerView.addOnItemTouchListener(new RecyclerTouchListener(getApplicationContext(), recyclerView, new ClickListener() {
+
+
+        /*recyclerView.addOnItemTouchListener(new RecyclerTouchListener(getApplicationContext(), recyclerView, new ClickListener() {
             @Override
             public void onClick(View view, int position) {
                 Notify notify = notifyList.get(position);
@@ -57,7 +63,7 @@ public class NotifyActivity extends AppCompatActivity {
             public void onLongClick(View view, int position) {
 
             }
-        }));
+        }));*/
 
         prepareNotifyData();
     }
@@ -106,7 +112,93 @@ public class NotifyActivity extends AppCompatActivity {
         mAdapter.notifyDataSetChanged();
     }
 
-    public interface ClickListener {
+    private void setUpItemTouchHelper() {
+
+        ItemTouchHelper.SimpleCallback simpleItemTouchCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+
+            // we want to cache these and not allocate anything repeatedly in the onChildDraw method
+            Drawable background;
+            Drawable xMark;
+            int xMarkMargin;
+            boolean initiated;
+
+            private void init() {
+                background = new ColorDrawable(Color.RED);
+                xMark = ContextCompat.getDrawable(NotifyActivity.this, R.drawable.smile);
+                xMark.setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_ATOP);
+                xMarkMargin = (int) NotifyActivity.this.getResources().getDimension(R.dimen.materialize_avatar);
+                initiated = true;
+            }
+
+            // not important, we don't want drag & drop
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public int getSwipeDirs(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
+                int position = viewHolder.getAdapterPosition();
+                NotifyAdapter notifyAdapter = (NotifyAdapter) recyclerView.getAdapter();
+                if (notifyAdapter.isUndoOn() && notifyAdapter.isPendingRemoval(position)) {
+                    return 0;
+                }
+                return super.getSwipeDirs(recyclerView, viewHolder);
+            }
+
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int swipeDir) {
+                int swipedPosition = viewHolder.getAdapterPosition();
+                NotifyAdapter adapter = (NotifyAdapter) recyclerView.getAdapter();
+                boolean undoOn = adapter.isUndoOn();
+                if (undoOn) {
+                    adapter.pendingRemoval(swipedPosition);
+                } else {
+                    adapter.remove(swipedPosition);
+                }
+            }
+
+            @Override
+            public void onChildDraw(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+                View itemView = viewHolder.itemView;
+
+                // not sure why, but this method get's called for viewholder that are already swiped away
+                if (viewHolder.getAdapterPosition() == -1) {
+                    // not interested in those
+                    return;
+                }
+
+                if (!initiated) {
+                    init();
+                }
+
+                // draw red background
+                background.setBounds(itemView.getRight() + (int) dX, itemView.getTop(), itemView.getRight(), itemView.getBottom());
+                background.draw(c);
+
+                // draw x mark
+                int itemHeight = itemView.getBottom() - itemView.getTop();
+                int intrinsicWidth = xMark.getIntrinsicWidth();
+                int intrinsicHeight = xMark.getIntrinsicWidth();
+
+                int xMarkLeft = itemView.getRight() - xMarkMargin - intrinsicWidth;
+                int xMarkRight = itemView.getRight() - xMarkMargin;
+                int xMarkTop = itemView.getTop() + (itemHeight - intrinsicHeight)/2;
+                int xMarkBottom = xMarkTop + intrinsicHeight;
+                xMark.setBounds(xMarkLeft, xMarkTop, xMarkRight, xMarkBottom);
+
+                xMark.draw(c);
+
+                super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+            }
+
+        };
+        ItemTouchHelper mItemTouchHelper = new ItemTouchHelper(simpleItemTouchCallback);
+        mItemTouchHelper.attachToRecyclerView(recyclerView);
+    }
+
+
+   /* public interface ClickListener {
         void onClick(View view, int position);
 
         void onLongClick(View view, int position);
@@ -153,5 +245,5 @@ public class NotifyActivity extends AppCompatActivity {
         public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
 
         }
-    }
+    }*/
 }
--- app/src/main/java/by/hmarka/alexey/incognito/ui/adapters/NotifyAdapter.java
+++ app/src/main/java/by/hmarka/alexey/incognito/ui/adapters/NotifyAdapter.java
@@ -1,11 +1,15 @@
 package by.hmarka.alexey.incognito.ui.adapters;
 
+import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import by.hmarka.alexey.incognito.R;
@@ -17,9 +21,20 @@ import by.hmarka.alexey.incognito.entities.Notify;
 public class NotifyAdapter extends RecyclerView.Adapter<NotifyAdapter.MyViewHolder> {
 
     private List<Notify> notifyList;
+    private List<Notify> itemsPendingRemoval;
+
+
+    private static final int PENDING_REMOVAL_TIMEOUT = 3000;
+    int lastInsertedIndex; // so we can add some more items for testing purposes
+    boolean undoOn; // is undo on, you can turn it on from the toolbar menu
+
+    HashMap<String, Runnable> pendingRunnables = new HashMap<>(); // map of items to pending runnables, so we can cancel a removal if need be
+
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
         public TextView description, preview, type, time;
+        Button undoButton;
+
 
         public MyViewHolder(View view) {
             super(view);
@@ -27,13 +42,20 @@ public class NotifyAdapter extends RecyclerView.Adapter<NotifyAdapter.MyViewHold
             preview = (TextView) view.findViewById(R.id.notify_preview);
             type = (TextView) view.findViewById(R.id.notify_pic);
             time = (TextView) view.findViewById(R.id.notify_time);
+
+
+            undoButton = (Button) itemView.findViewById(R.id.undo_button);
         }
     }
 
 
     public NotifyAdapter(List<Notify> notifyList) {
         this.notifyList = notifyList;
+        itemsPendingRemoval = new ArrayList<>();
+        // let's generate some items
+        lastInsertedIndex = 15;
     }
+
 
     @Override
     public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -45,15 +67,88 @@ public class NotifyAdapter extends RecyclerView.Adapter<NotifyAdapter.MyViewHold
 
     @Override
     public void onBindViewHolder(MyViewHolder holder, int position) {
-        Notify notify = notifyList.get(position);
-        holder.description.setText(notify.getTitle());
+        MyViewHolder viewHolder = (MyViewHolder)holder;
+
+        final Notify notify = notifyList.get(position);
+
+        /*holder.description.setText(notify.getTitle());
         holder.preview.setText(notify.getPreview());
         holder.type.setText(notify.getType());
-        holder.time.setText(notify.getTime());
+        holder.time.setText(notify.getTime());*/
+
+        if (itemsPendingRemoval.contains(notify)) {
+            // we need to show the "undo" state of the row
+        holder.itemView.setBackgroundColor(Color.RED);
+        holder.description.setVisibility(View.GONE);
+        holder.undoButton.setVisibility(View.VISIBLE);
+        holder.undoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // user wants to undo the removal, let's cancel the pending task
+                Runnable pendingRemovalRunnable = pendingRunnables.get(notify);
+                pendingRunnables.remove(notify);
+                if (pendingRemovalRunnable != null)
+                  //  handler.removeCallbacks(pendingRemovalRunnable);
+                itemsPendingRemoval.remove(notify);
+                // this will rebind the row in "normal" state
+                notifyItemChanged(notifyList.indexOf(notify));
+            }
+        });
+    } else {
+        // we need to show the "normal" state
+        holder.itemView.setBackgroundColor(Color.WHITE);
+        holder.description.setVisibility(View.VISIBLE);
+        holder.description.setText("notify");
+        holder.undoButton.setVisibility(View.GONE);
+        holder.undoButton.setOnClickListener(null);
     }
+    }
+
+    public void setUndoOn(boolean undoOn) {
+        this.undoOn = undoOn;
+    }
+
+    public boolean isUndoOn() {
+        return undoOn;
+    }
+
 
     @Override
     public int getItemCount() {
         return notifyList.size();
+    }
+
+    public void pendingRemoval(int position) {
+        final Notify notify = notifyList.get(position);
+        if (!itemsPendingRemoval.contains(notify)) {
+            itemsPendingRemoval.add(notify);
+            // this will redraw row in "undo" state
+            notifyItemChanged(position);
+            // let's create, store and post a runnable to remove the item
+            Runnable pendingRemovalRunnable = new Runnable() {
+                @Override
+                public void run() {
+                    remove(notifyList.indexOf(notify));
+                }
+            };
+            //handler.postDelayed(pendingRemovalRunnable, PENDING_REMOVAL_TIMEOUT);
+            //pendingRunnables.put(notify, pendingRemovalRunnable);
+        }
+    }
+
+    public void remove(int position) {
+        Notify notify = notifyList.get(position);
+        if (itemsPendingRemoval.contains(notify)) {
+            itemsPendingRemoval.remove(notify);
+        }
+        if (notifyList.contains(notify)) {
+            notifyList.remove(position);
+            notifyItemRemoved(position);
+        }
+    }
+
+    public boolean isPendingRemoval(int position) {
+        Notify notify = notifyList.get(position);
+        return itemsPendingRemoval.contains(notify);
     }
 }
